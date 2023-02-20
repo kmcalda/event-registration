5763d74e5ac64c25c1c2b697984fcd970d23da08
Kevin Marvin Calda <caldakevinmarvin@yahoo.com>
2023-02-20 18:16:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 let employee_number = document.getElementById("employee_number");
-    let employee_name = document.getElementById("employee_name");
-    let department = document.getElementById("department");
-    document.getElementById("button").addEventListener("click", () => {
-        if (employee_name.value && employee_number.value) {
-            let value = `${employee_number.value}\n${employee_name.value}\n${department.value}`
-            let qrcode = document.getElementById("qrcode");
-            qrcode.innerHTML = "";
-            new QRCode(qrcode, value);
-            document.getElementById("qr-height").style.cssText += "max-height:500px;"
-        }
-        else {
-            alert("Please fill all field!")
-        }
-        
-    })
+let employee_name = document.getElementById("employee_name");
+let department = document.getElementById("department");
+let qrHeight = document.getElementById("qr-height");
+document.getElementById("button").addEventListener("click", () => {
+    const qrcode = document.getElementById("qrcode");
+    qrcode.innerHTML = ""
+    if (employee_name.value && employee_number.value) {
+        let data = `${employee_number.value}\n${employee_name.value}\n${department.value}`
+        const qrCode = new QRCodeStyling({
+            type: "svg",
+            data: data,
+            image: "img/pioneerlogo.png",
+            dotsOptions: {
+                type: "rounded"
+            },
+            imageOptions: {
+                crossOrigin: "anonymous",
+                margin: 10
+            }
+        });
+        qrCode.append(qrcode);
+        qrHeight.style.cssText += "max-height:500px;"
+    }
+    else {
+        alert("Please fill all field!")
+    }
+    
+})
